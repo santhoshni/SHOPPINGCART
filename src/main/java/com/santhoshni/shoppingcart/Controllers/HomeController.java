3d84ab391d36fc364a9ec352ec7807d19522d30b
santhoshni <70932006+santhoshni@users.noreply.github.com>
2021-04-10 13:27:07 -0500

    InitialCommitCode

---
+++ src/main/java/com/santhoshni/shoppingcart/Controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.santhoshni.shoppingcart.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+@GetMapping("/")    
+public String home(){
+    return "home";
+
+}
+    
+}
