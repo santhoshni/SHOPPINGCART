1e7fe918c14bccc8e60c9073b6dc134c8b47705e
Santhoshni <dsanthoshni@gmail.com>
2021-04-17 20:46:30 -0500

    Addeds admin page work

---
+++ src/main/java/com/santhoshni/shoppingcart/models/PageRepository.java
@@ -0,0 +1,23 @@
+package com.santhoshni.shoppingcart.models;
+
+import java.util.List;
+
+import com.santhoshni.shoppingcart.models.data.Page;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PageRepository extends JpaRepository<Page,Integer> {
+   
+    //Page findBySlug(String slug);
+
+    Page findBySlug(String slug);
+
+    // @Query("SELECT p FROM Page p WHERE p.id != :id and p.slug = :slug")
+    // Page findBySlug(int id, String slug);
+
+    Page findBySlugAndIdNot(String slug, int id);
+
+    List<Page> findAllByOrderBySortingAsc();
+    
+}
